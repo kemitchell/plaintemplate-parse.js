5ba7d1ec039e3013b74cb8f1a08af172d209c828
Kyle Mitchell <kyle@kemitchell.com>
2015-12-08 18:29:01 -0800

    Test positions on multiple lines

--- test.js
+++ test.js
@@ -28,4 +28,19 @@ tape(function(test) {
         position: { line: 1, column: 26 },
         content: [ ] } ])
 
+  test.deepEqual(
+    plaintemplate('Hello <% insert name%>!\nHow is <% insert state %>?'),
+    [ { text: 'Hello ',
+        position: { line: 1, column: 1 } },
+      { tag: [ 'insert', 'name' ],
+        position: { line: 1, column: 7 },
+        content: [ ] },
+      { text: '!\nHow is ',
+        position: { line: 1, column: 23 } },
+      { tag: [ 'insert', 'state' ],
+        position: { line: 2, column: 8 },
+        content: [ ] },
+      { text: '?',
+        position: { line: 2, column: 26 } } ])
+
   test.end() })
