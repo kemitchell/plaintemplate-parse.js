c357285d10b11e31230ee88128f94ef855c7ef2e
Kyle E. Mitchell <kyle@kemitchell.com>
2016-07-15 16:00:30 -0700

    Use JavaScript Standard Style

--- index.js
+++ index.js
@@ -2,36 +2,41 @@ module.exports = plaintemplate
 
 var STARTS_WITH_NEWLINE = /^(\n\r?)/
 
-function plaintemplate(input, delimiters) {
+function plaintemplate (input, delimiters) {
   if (delimiters === undefined) {
     delimiters = {
       open: '<%',
       close: '%>',
       start: '{',
-      end: '}' } }
+      end: '}'
+    }
+  }
 
   var openLength = delimiters.open.length
   var closeLength = delimiters.close.length
 
-  var lookahead = memoize(function(length) {
-    return function(index) {
-      return input.substr(index, length) } })
+  var lookahead = memoize(function (length) {
+    return function (index) {
+      return input.substr(index, length)
+    }
+  })
 
   var openLookahead = lookahead(openLength)
   var closeLookahead = lookahead(closeLength)
   var newlineLookahead = lookahead(2)
 
   // The array of tokens to be returned.
-  var output = [ ]
+  var output = []
 
   // A stack of content arrays, like output.
-  var contentArrayStack = [ output ]
+  var contentArrayStack = [output]
 
   // The element at index zero is the content array of the current tag, if any.
   // Subsequent elements are the content arrays of containing tags, and,
   // finally, `output`.
-  function currentStack() {
-    return contentArrayStack[0] }
+  function currentStack () {
+    return contentArrayStack[0]
+  }
 
   // The current parser state.
   var inTag = false
@@ -45,45 +50,55 @@ function plaintemplate(input, delimiters) {
   var column = 1 // Begin at 1.
 
   // Append a string to the last text token.
-  function appendString(string) {
+  function appendString (string) {
     var current = currentStack()
     var length = current.length
     if (length > 0) {
       var last = current[length - 1]
       if ('text' in last) {
-        last.text = ( last.text + string )
-        return } }
+        last.text = last.text + string
+        return
+      }
+    }
     current.push({
       text: string,
-      position: currentPosition() }) }
+      position: currentPosition()
+    })
+  }
 
   // Returns the last tag token.
-  function currentTag() {
+  function currentTag () {
     var stack = currentStack()
-    return stack[stack.length - 1] }
+    return stack[stack.length - 1]
+  }
 
   // Advances position counters by a certain number of characters.
-  function advance(length) {
+  function advance (length) {
     index += length
-    column += length }
+    column += length
+  }
 
   // Create a position object at the current parser position.
-  function currentPosition() {
-    return { line: line, column: column } }
+  function currentPosition () {
+    return {line: line, column: column}
+  }
 
-  var error, match, lineBreak, tag
+  var error
+  var match
+  var lineBreak
+  var tag
 
   // Iterate through the characters of the input.
-  while(index < length) {
+  while (index < length) {
     // Not within a tag.
     if (!inTag) {
       // Are we at the start of a tag?
       if (openLookahead(index) === delimiters.open) {
         inTag = true
-        currentStack().push({ tag: '', position: currentPosition() })
-        advance(openLength) }
+        currentStack().push({tag: '', position: currentPosition()})
+        advance(openLength)
       // Not at the start of a tag.
-      else {
+      } else {
         // Is this a newline?
         match = STARTS_WITH_NEWLINE.exec(newlineLookahead(index))
         if (match) {
@@ -91,13 +106,15 @@ function plaintemplate(input, delimiters) {
           appendString(lineBreak)
           line++
           column = 1
-          index += lineBreak.length }
+          index += lineBreak.length
         // Just text.
-        else {
+        } else {
           appendString(input[index])
-          advance(1) } } }
+          advance(1)
+        }
+      }
     // Within a tag.
-    else /* inTag */ {
+    } else /* inTag */ {
       // Are we at the end of the tag?
       tag = currentTag()
       if (closeLookahead(index) === delimiters.close) {
@@ -105,60 +122,73 @@ function plaintemplate(input, delimiters) {
         if (endsWith(tag.tag, delimiters.start)) {
           inTag = false
           tag.tag = tag.tag
-            .substring(0, tag.tag.lastIndexOf(delimiters.start) - 1)
-            .trim()
-          tag.content = [ ]
-          contentArrayStack.unshift(tag.content) }
+          .substring(0, tag.tag.lastIndexOf(delimiters.start) - 1)
+          .trim()
+          tag.content = []
+          contentArrayStack.unshift(tag.content)
         // End of a continuing tag.
-        else if (endsWith(tag.tag, delimiters.end)) {
+        } else if (endsWith(tag.tag, delimiters.end)) {
           if (contentArrayStack.length > 1) {
             inTag = false
             tag.tag = tag.tag.trim()
             // Do not include a token for the ending tag.
             currentStack().pop()
-            contentArrayStack.shift() }
-          else {
+            contentArrayStack.shift()
+          } else {
             error = new Error(
-              'No tag to end at '+
+              'No tag to end at ' +
               'line ' + tag.position.line + ', ' +
               'column ' + tag.position.column)
             error.position = tag.position
-            throw error } }
+            throw error
+          }
         // End of an insert tag.
-        else {
+        } else {
           tag.tag = tag.tag.trim()
-          inTag = false }
-        advance(closeLength) }
+          inTag = false
+        }
+        advance(closeLength)
       // Text within the tag.
-      else {
-        tag.tag = ( tag.tag + input[index])
-        advance(1) } } }
+      } else {
+        tag.tag = tag.tag + input[index]
+        advance(1)
+      }
+    }
+  }
   if (contentArrayStack.length > 1) {
     contentArrayStack.shift()
     tag = currentTag()
     error = new Error(
       'Unclosed tag at ' +
       'line ' + tag.position.line + ', ' +
-      'column ' + tag.position.column)
+      'column ' + tag.position.column
+    )
     error.position = tag.position
-    throw error }
-  return output }
+    throw error
+  }
+  return output
+}
 
 // Memoize a function that takes a single argument that can be used as keys of
 // an object.
-function memoize(f) {
-  var cache = { }
-  return function(argument) {
+function memoize (f) {
+  var cache = {}
+  return function (argument) {
     if (cache[argument]) {
-      return cache[argument] }
-    else {
+      return cache[argument]
+    } else {
       var result = f(argument)
       cache[argument] = result
-      return result } } }
+      return result
+    }
+  }
+}
 
-function endsWith(string, suffix) {
+function endsWith (string, suffix) {
   var trimmed = string.trim()
   var index = trimmed.lastIndexOf(suffix)
   return (
     index > -1 &&
-    ( index === trimmed.length - suffix.length ) ) }
+    (index === trimmed.length - suffix.length)
+  )
+}
--- test.js
+++ test.js
@@ -1,84 +1,148 @@
 var tape = require('tape')
 var plaintemplate = require('./')
 
-tape('plaintemplate-parse', function(test) {
+tape('plaintemplate-parse', function (test) {
   test.deepEqual(
     plaintemplate('Just text. No templating.'),
-    [ { text: 'Just text. No templating.',
-        position: { line: 1, column: 1 } } ],
-    'just text')
+    [
+      {
+        text: 'Just text. No templating.',
+        position: {line: 1, column: 1}
+      }
+    ],
+    'just text'
+  )
 
   test.deepEqual(
     plaintemplate('Hello <% insert name %>!'),
-    [ { text: 'Hello ',
-        position: { line: 1, column: 1 } },
-      { tag: 'insert name',
-        position: { line: 1, column: 7 } },
-      { text: '!',
-        position: { line: 1, column: 24 } } ],
-    'insert')
+    [
+      {
+        text: 'Hello ',
+        position: {line: 1, column: 1}
+      },
+      {
+        tag: 'insert name',
+        position: {line: 1, column: 7}
+      },
+      {
+        text: '!',
+        position: {line: 1, column: 24}
+      }
+    ],
+    'insert'
+  )
 
   test.deepEqual(
     plaintemplate('Hello <% insert first %> <%insert last%>'),
-    [ { text: 'Hello ',
-        position: { line: 1, column: 1 } },
-      { tag: 'insert first',
-        position: { line: 1, column: 7 } },
-      { text: ' ',
-        position: { line: 1, column: 25 } },
-      { tag: 'insert last',
-        position: { line: 1, column: 26 } } ],
-    'two inserts')
+    [
+      {
+        text: 'Hello ',
+        position: {line: 1, column: 1}
+      },
+      {
+        tag: 'insert first',
+        position: {line: 1, column: 7}
+      },
+      {
+        text: ' ',
+        position: {line: 1, column: 25}
+      },
+      {
+        tag: 'insert last',
+        position: {line: 1, column: 26}
+      }
+    ],
+    'two inserts'
+  )
 
   test.deepEqual(
     plaintemplate('Hello <% insert name%>!\nHow is <% insert state %>?'),
-    [ { text: 'Hello ',
-        position: { line: 1, column: 1 } },
-      { tag: 'insert name',
-        position: { line: 1, column: 7 } },
-      { text: '!\nHow is ',
-        position: { line: 1, column: 23 } },
-      { tag: 'insert state',
-        position: { line: 2, column: 8 } },
-      { text: '?',
-        position: { line: 2, column: 26 } } ],
-    'two lines')
+    [
+      {
+        text: 'Hello ',
+        position: {line: 1, column: 1}
+      },
+      {
+        tag: 'insert name',
+        position: {line: 1, column: 7}
+      },
+      {
+        text: '!\nHow is ',
+        position: {line: 1, column: 23}
+      },
+      {
+        tag: 'insert state',
+        position: {line: 2, column: 8}
+      },
+      {
+        text: '?',
+        position: {line: 2, column: 26}
+      }
+    ],
+    'two lines'
+  )
 
   test.deepEqual(
     plaintemplate('<% if onsale { %>Price: $<% insert price %><% } %>'),
-    [ { tag: 'if onsale',
-        position: { line: 1, column: 1 },
+    [
+      {
+        tag: 'if onsale',
+        position: {line: 1, column: 1},
         content: [
-          { text: 'Price: $',
-            position: { line: 1, column: 18 } },
-          { tag: 'insert price',
-            position: { line: 1, column: 26 } } ] } ],
-    'if then insert')
+          {
+            text: 'Price: $',
+            position: {line: 1, column: 18}
+          },
+          {
+            tag: 'insert price',
+            position: {line: 1, column: 26}
+          }
+        ]
+      }
+    ],
+    'if then insert'
+  )
 
   test.deepEqual(
     plaintemplate(
       '{{ if onsale start }}Price: ${{ insert price }}{{ end }}',
-      { open: '{{',
+      {
+        open: '{{',
         close: '}}',
         start: 'start',
-        end: 'end' }),
-    [ { tag: 'if onsale',
-        position: { line: 1, column: 1 },
+        end: 'end'
+      }
+    ),
+    [
+      {
+        tag: 'if onsale',
+        position: {line: 1, column: 1},
         content: [
-          { text: 'Price: $',
-            position: { line: 1, column: 22 } },
-          { tag: 'insert price',
-            position: { line: 1, column: 30 } } ] } ],
-    'mustache syntax')
+          {
+            text: 'Price: $',
+            position: {line: 1, column: 22}
+          },
+          {
+            tag: 'insert price',
+            position: {line: 1, column: 30}
+          }
+        ]
+      }
+    ],
+    'mustache syntax'
+  )
 
   test.throws(
-    function() { plaintemplate('This is <% } %> invalid.') },
+    function () { plaintemplate('This is <% } %> invalid.') },
     /No tag to end at line 1, column 9/,
-    'orphan closing tag throws')
+    'orphan closing tag throws'
+  )
 
   test.throws(
-    function() { plaintemplate('This is <% if x { %> invalid.') },
+    function () { plaintemplate('This is <% if x { %> invalid.') },
     /Unclosed tag at line 1, column 9/,
-    'orphan opening tag throws')
+    'orphan opening tag throws'
+  )
 
-  test.end() })
+  test.end()
+})
